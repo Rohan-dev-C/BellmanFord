cd0df535db36a3b46718b7a21449b7e29310a071
Rohan.sampath <Rohan.sampath@GMR-118-2-1>
2022-12-03 14:32:40 -0800

    Add project files.

---
+++ BellmanFord/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+
+namespace BellmanFord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IComparer<int> comparer = Comparer<int>.Default;
+            Graph<int> graph = new Graph<int>(comparer);  
+            Console.WriteLine("Hello, World!");
+
+
+            graph.AddVertex(1);
+            graph.AddVertex(2);
+            graph.AddVertex(3);
+            graph.AddVertex(4);
+            graph.AddVertex(5);
+            graph.AddVertex(6);
+            graph.AddEdge(1, 2, 1); 
+            graph.AddEdge(2, 3, 1); 
+            graph.AddEdge(3, 4, -1); 
+            graph.AddEdge(4, 5, -1); 
+            graph.AddEdge(3, 5, -1); 
+            graph.AddEdge(4, 6, 1);
+
+
+            var result = Pathfinding<int>.BellmanFord(graph); 
+        }
+    }
+}
